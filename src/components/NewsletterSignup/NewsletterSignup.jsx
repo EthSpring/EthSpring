8a82cf8b0436121f59ddda0bb1c1a4acd491d87f
Katz <mykatz01@gmail.com>
2021-04-19 17:54:29 -0700

    Make newsletter signup work

--- src/components/NewsletterSignup/NewsletterSignup.jsx
+++ src/components/NewsletterSignup/NewsletterSignup.jsx
@@ -14,15 +14,12 @@ export default function HoverBox() {
             </div>
           </div>
           <div className="col col--6">
-            <div>
+            <form action="https://tinyletter.com/ethspring" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/ethspring', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
               <div className={styles.NewsletterSignupInputContainer}>
-                <input type="text" className={styles.NewsletterSignupInput} placeholder="Name"></input>
-              </div>
-              <div className={styles.NewsletterSignupInputContainer}>
-                <input type="text" className={styles.NewsletterSignupInput} placeholder="Email Address"></input>
+                <input type="text" name="email" className={styles.NewsletterSignupInput} placeholder="Email Address"></input>
               </div>
               <button class="button button--primary button--block">Submit</button>
-            </div>
+            </form>
           </div>
         </div>
       </div>
