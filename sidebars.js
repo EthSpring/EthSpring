09ff31df68312f8b18042686fc3e2f3310c099a8
Katz <mykatz01@gmail.com>
2021-04-19 17:49:09 -0700

    Add peer protocols page

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,11 @@ module.exports = {
   docs: [
     {
       type: 'category',
+      label: 'Peer Protocols',
+      items: ['peer-protocols']
+    },
+    {
+      type: 'category',
       label: 'Docusaurus Tutorial',
       items: [
         'getting-started',
