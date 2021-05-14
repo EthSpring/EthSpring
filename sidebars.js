e39ecb742f209a6dd6c35b254251702a500c57d2
Evan Mays <me@evanmays.com>
2021-05-13 19:13:40 -0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -93,7 +93,6 @@ module.exports = {
       items: [
         "ethereum-applications/0-preface",
         "ethereum-applications/1-erc-20-tokens",
-        "ethereum-applications/2-stablecoins",
         "ethereum-applications/3-nfts-collectibles-socks",
         "ethereum-applications/4-games",
         "ethereum-applications/5-finance-and-investing",
